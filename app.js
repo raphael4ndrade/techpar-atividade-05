f34f897c808fb8cea66771ae2a97198b06334eb2
raphael4ndrade <raphael4ndrade@gmail.com>
2017-04-05 22:40:14 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const knex = require('./db');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static("public"));
+// app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
+app.use(bodyParser.json());
+
+app.get('/list', (req, res) => {
+    knex("pessoa").select().then((ret) => res.send(ret).status(200));
+});
+
+app.post('/save', (req, res) => {
+    let pessoa = req.body;
+    knex("pessoa").insert(pessoa, 'id_pessoa')
+        .then((ret) => {
+            pessoa.id_pessoa = ret[0];
+            res.send(pessoa).status(200);
+        }).catch((err) => {
+            console.log(err);
+            res.send().status(500);
+        });
+});
+
+knex.migrate.latest();
+
+module.exports = app;
